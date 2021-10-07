7f0aacd53bbe7b3133af63c437b4a3a501b6a74c
Tom Silverstein <tsilver@resequence.com>
2021-10-07 00:22:14 -0400

    No tag

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Version 1.0.9")
+	fmt.Println("Version 1.0.9 - no tag")
 }
--- v2/main.go
+++ v2/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Version 2.1.9")
+	fmt.Println("Version 2.1.9 - no tag")
 }
